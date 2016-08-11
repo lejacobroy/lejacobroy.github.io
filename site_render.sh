44f9a2a3469d60f8e3db5fbf35403bc7fa82ea1a
Jacob Roy <moi@lejacobroy.com>
2016-08-11 10:40:55 -0400

    updated css

--- site_render.sh
+++ site_render.sh
@@ -1,1 +1,1 @@
-cd ~/Dropbox/Jekyll_blog/ && bundle exec jekyll build && cd ~/Dropbox/Jekyll_blog/_site/ && find . -type f -name '*.html' -exec sed -i '' 's/\<img\ src\=/\<img\ src\=\"https\:\/\/jacobroy\.quebec\/public\/img\/load\.png\"\ data\-src\=/g' {} +
+cd /Volumes/Gringo/Dropbox/Jekyll_blog/ && bundle exec jekyll build && cd /Volumes/Gringo/Dropbox/Jekyll_blog/_site/ && find . -type f -name '*.html' -exec sed -i '' 's/\<img\ src\=/\<img\ src\=\"https\:\/\/jacobroy\.quebec\/public\/img\/load\.png\"\ data\-src\=/g' {} +
