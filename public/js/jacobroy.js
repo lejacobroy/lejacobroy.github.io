b590adca254733dd76ddb63545b65f91c6d4138c
Jacob Roy <moi@lejacobroy.com>
2016-08-19 12:51:14 -0400

    unveill 200

--- public/js/jacobroy.js
+++ public/js/jacobroy.js
@@ -3,7 +3,7 @@ var host = "jacobroy.quebec";
         window.location.protocol = "https";
 
     $(document).ready(function() {
-    $("img").unveil(1, function() {
+    $("img").unveil(200, function() {
       $(this).load(function() {
       this.style.opacity = 1;
       });
